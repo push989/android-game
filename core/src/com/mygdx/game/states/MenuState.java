7babee92c4401bc0ca4ffe6946019990611838df
Rok Pusnik <r.puska@gmail.com>
2016-05-21 21:11:24 +0200

    initial commit

---
+++ core/src/com/mygdx/game/states/MenuState.java
@@ -0,0 +1,43 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.MyGdxGame;
+
+/**
+ * Created by Rok on 21. 05. 2016.
+ */
+public class MenuState extends State {
+    private Texture background;
+    private Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0,0, MyGdxGame.WIDTH, MyGdxGame.HEIGHT);
+        sb.draw(playBtn, (MyGdxGame.WIDTH / 2) - (playBtn.getWidth() / 2), MyGdxGame.HEIGHT / 2);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        playBtn.dispose();
+    }
+}
