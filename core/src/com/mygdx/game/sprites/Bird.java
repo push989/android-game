a96e6a79790f53835a66888b633293b012c2680f
Rok Pusnik <r.puska@gmail.com>
2016-05-21 22:38:23 +0200

    Added tube obstacles and bird jump

--- core/src/com/mygdx/game/sprites/Bird.java
+++ core/src/com/mygdx/game/sprites/Bird.java
@@ -20,9 +20,12 @@ public class Bird {
     }
 
     public void update(float dt) {
-        velocity.add(0, GRAVITY, 0);
+        if(position.y > 0)
+            velocity.add(0, GRAVITY, 0);
         velocity.scl(dt);
         position.add(0, velocity.y, 0);
+        if(position.y < 0)
+            position.y = 0;
 
         velocity.scl(1 / dt);
     }
@@ -33,5 +36,9 @@ public class Bird {
 
     public Texture getBird() {
         return bird;
+    }
+
+    public void jump() {
+        velocity.y = 250;
     }
 }
--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -1,25 +1,33 @@
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.sprites.Bird;
+import com.mygdx.game.sprites.Tube;
 
 /**
  * Created by Rok on 21. 05. 2016.
  */
 public class PlayState extends State {
     private Bird bird;
+    private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, MyGdxGame.WIDTH / 2, MyGdxGame.HEIGHT / 2);
+        bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
     protected void handleInput() {
-
+        if(Gdx.input.justTouched()) {
+            bird.jump();
+        }
     }
 
     @Override
@@ -32,7 +40,10 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
