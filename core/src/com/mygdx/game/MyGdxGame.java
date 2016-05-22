9d7117acdd87b96da1d93023419860adb10fa4d4
Rok Pusnik <r.puska@gmail.com>
2016-05-22 19:53:16 +0200

    added music and sound effects

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -15,12 +16,19 @@ public class MyGdxGame extends ApplicationAdapter {
 	public static final String TITLE = "fappy bird";
 	private GameStateManager gsm;
 	private SpriteBatch batch;
-	Texture img;
+
+	private Music music;
+
+	//Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
+		music = Gdx.audio.newMusic(Gdx.files.internal(("music.mp3")));
+		music.setLooping(true);
+		music.setVolume(0.1f);
+		music.play();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		gsm.push(new MenuState(gsm));
 
@@ -35,5 +43,11 @@ public class MyGdxGame extends ApplicationAdapter {
 		/*batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();*/
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		music.dispose();
 	}
 }
